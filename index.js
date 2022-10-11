a69ea898bbe31e806be9d4607201d78aec6ec045
Leandro Nazar <ljnazar@gmail.com>
2022-10-11 14:09:55 -0300

    Test_Vercel_get-json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import path from 'path';
 //const express = require('express');
 //import path from 'path';
 //const path = require('path');
@@ -42,9 +43,9 @@ export default function handler(req, res) {
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  //const jsonDirectory = path.join(process.cwd(), 'json');
+  const jsonDirectory = path.join(process.cwd(), 'json');
   //Read the json data file data.json
-  const fileContents = await fs.readFile('table-score.json', 'utf8');
+  const fileContents = await fs.readFile(jsonDirectory + 'table-score.json', 'utf8');
   //Return the content of the data file in json format
   res.status(200).json(fileContents);
 }
