0f6650a2f3c7248c6d134dc35091ba53402f1ed5
Leandro Nazar <ljnazar@gmail.com>
2022-10-11 18:40:21 -0300

    Test_Vercel_get-json

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 
 //const jsonDirectory = path.join(process.cwd(), 'json');
 
+/*
 let obj;
 let json;
 
@@ -33,7 +34,8 @@ app.get('/tableScore', (req, res) => {
     //res.json({nombre: "Lean"});
     //res.json(json); // object string
     res.end(json);
-});
+});*/
+
 /*
 export default function handler(req, res) {
 //export default (req, res) => {
@@ -86,6 +88,28 @@ app.post('/profile', function (req, res, next) {
   console.log(req.body)
   res.json(req.body)
 })*/
+
+
+
+
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+
+
+
+
 
 app.listen(port, () => {
     console.log('Server up');
