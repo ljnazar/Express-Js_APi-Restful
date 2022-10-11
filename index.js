013ba40b6dd22c3d792853a33fafa363a1e42ba3
Leandro Nazar <ljnazar@gmail.com>
2022-10-11 18:45:23 -0300

    Test_Vercel_get-json

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'application/json');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
