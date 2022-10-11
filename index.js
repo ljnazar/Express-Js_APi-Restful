2a6fc0ac6be81e2e306c5a4689d17c0e9beedad3
Leandro Nazar <ljnazar@gmail.com>
2022-10-11 20:50:56 -0300

    Test_Vercel_get-json

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   //res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   //asd = res.json(json);
-  //qwe = res.json(obj);
+  qwe = res.json(obj);
   //res.end(`obj: ${asd} - string: ${qwe}`);
 });
 /*
