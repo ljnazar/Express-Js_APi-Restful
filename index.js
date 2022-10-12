d90d7b69ddc6f7552451bbb4c4dbf5f9038a33fa
Leandro Nazar <ljnazar@gmail.com>
2022-10-12 09:04:18 -0300

    Add_CORS

--- index.js
+++ index.js
@@ -106,6 +106,28 @@ app.get('/api/getData', (req, res) => {
 
 
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
 
 
 
@@ -115,4 +137,6 @@ app.listen(port, () => {
 });
 //app.listen(process.env.PORT || 3000);
 
-module.exports = app;
+//module.exports = app;
+
+module.exports = allowCors(app);
